8817539a261b18f4272838b92f6205fa258c3128
lhc37 <lhc37@bath.ac.uk>
2019-12-24 19:58:03 +0000

    finish one element test

--- cpp/test/removeNthFromEnd_t.cpp
+++ cpp/test/removeNthFromEnd_t.cpp
@@ -10,10 +10,26 @@ void Test_removeNthFromEnd() {
   int k = 2;
   int arr[] = {1,2,3,4,5};
   ListNode* testcase = arrayToList(arr, 5); 
-  removeNthFromEnd(testcase, k);
+  testcase = removeNthFromEnd(testcase, k);
   int g[] = {1,2,3,5};
   ListNode* goal = arrayToList(g, 4); 
   assert(cmpTwoLinkedLists(&testcase, &goal));
+
+  /* LeetCode Test */
+  k = 3;
+  int arr_1[] = {1,2,3,4,5};
+  testcase = arrayToList(arr_1, 5); 
+  testcase = removeNthFromEnd(testcase, k);
+  int g_1[] = {1,2,4,5};
+  goal = arrayToList(g_1, 4); 
+  assert(cmpTwoLinkedLists(&testcase, &goal));
+
+  /* One Elem Test */
+  k = 1;
+  int arr_2[] = {1};
+  testcase = arrayToList(arr_2, 1); 
+  testcase = removeNthFromEnd(testcase, k);
+  assert(testcase == NULL);
 
   return;
 }
