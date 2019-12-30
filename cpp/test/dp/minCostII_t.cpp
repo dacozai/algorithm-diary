2bd9b4e0b5a779e891ac3e243b55fccbec1a9910
lhc37 <lhc37@bath.ac.uk>
2019-12-30 18:57:37 +0000

    question 265 testcases

--- cpp/test/dp/minCostII_t.cpp
+++ cpp/test/dp/minCostII_t.cpp
@@ -4,10 +4,26 @@
 */
 
 void Test_minCostII() {
-  /* Empty Test */
+  /* LeetCode Test */
   std::vector<std::vector<int>> vec = {{1,5,3},{2,9,4}};
   int ans =  minCostII(vec);
   assert(ans == 5);
+
+  /* Two options case */
+  vec = {{1,2},{3,4}};
+  ans =  minCostII(vec);
+  assert(ans == 5);
+
+  /* empty Test */
+  vec = {};
+  ans =  minCostII(vec);
+  assert(ans == 0);
+
+  /* Complex Case */
+  vec = {{1,2,5,3,7,9},{10,6,3,4,5,6},{14,19,12,17,14,18}};
+  ans =  minCostII(vec);
+  assert(ans == 17);
+
 
 
   return;
