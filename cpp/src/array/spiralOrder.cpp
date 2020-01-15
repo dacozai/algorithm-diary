66ef986264c5f1e01d3ec9460c95a41bb13577e5
lhc37 <eden.chen@bath.edu>
2020-01-15 00:04:28 +0000

    add spiral matrix

--- cpp/src/array/spiralOrder.cpp
+++ cpp/src/array/spiralOrder.cpp
@@ -1,35 +1,29 @@
 #include "test.h"
 /** Question no 54. medium Spiral Matrix
  * Author : Li-Han, Chen; 陳立瀚
- * Date   : 10th, January, 2020
+ * Date   : 15th, January, 2020
  * Source : https://leetcode.com/problems/spiral-matrix/
  * 
  * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
  * 
 */
 
-/** Solution [NOT DONE]
- * Runtime 4 ms	MeMory 8.6 MB; 
- * faster than 98.82%, less than 100% 
+/** Solution
+ * Runtime 0 ms	MeMory 8.5 MB; 
+ * faster than 100.00%, less than 100% 
  * O(n) ; O(n)
 */
 
 std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
   int delta[4][2] = {{0,1}, {1,0}, {0,-1},{-1,0}};
   std::vector<int> v;
-  int *p = new int[2];
   int R_L = matrix.size();
   if (R_L == 0) return v;
   int C_L = matrix[0].size();
-  int R_B = 0, C_B = 0;
-  p[0] = R_L/2; 
-  p[1] = C_L/2;
-  int i=0,j=0, k=0;
-  while(i!=p[0] || j!=p[1]) {
-    // cout << i << " and " << j << endl;
+  int R_B = 0, C_B = 0, CELLS = R_L*C_L;
+  int i=0,j=0,k=0, add_up=0;
+  
+  while(add_up < CELLS) {
     v.push_back(matrix[i][j]);
     if (i+delta[k%4][0] >= R_L) {
       C_L--;
@@ -47,9 +41,8 @@ std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
     
     i+=delta[k%4][0];
     j+=delta[k%4][1];
+    add_up++;
   }
-  
-  v.push_back(matrix[i][j]);
-  
+    
   return v;
 }
