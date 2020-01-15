edf8ae3865eaca9343d2d9d97ed5538ba478032d
lhc37 <eden.chen@bath.edu>
2020-01-15 14:34:07 +0000

    Q 1300

---
+++ cpp/src/array/findBestValue.cpp
@@ -0,0 +1,35 @@
+#include "test.h"
+/** Question no 1300. medium Sum of Mutated Array Closest to Target
+ * Author : Li-Han, Chen; 陳立瀚
+ * Date   : 15th, January, 2020
+ * Source : https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/
+ * 
+*/
+
+/** Solution
+ * Runtime 4 ms	MeMory 9.5 MB; 
+ * faster than 100.00%, less than 100% 
+ * O(n) ; O(1)
+*/
+
+int findBestValue(std::vector<int>& arr, int target) {
+  /* Speed Up */
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  /* Varialbes */
+  int LEN = arr.size(), ANS, Total_gap=0, smaller_nums=0;
+  double tmp_ans = (double)target/LEN;
+  
+  ANS = (tmp_ans-(int)tmp_ans)>0.5?tmp_ans+1:tmp_ans;
+  for (auto e: arr) {
+    if (e <= ANS) {
+      Total_gap += ANS-e;
+      smaller_nums++;
+    }
+  }
+
+  if (smaller_nums == 0) return ANS;
+  tmp_ans = (double) (abs(target-ANS*LEN)+Total_gap)/(LEN-smaller_nums);
+  return ANS+tmp_ans;
+}
