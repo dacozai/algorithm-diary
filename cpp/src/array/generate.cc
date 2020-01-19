badba266a69429258e281ad814dbaa99541ddec5
lhc37 <eden.chen@bath.edu>
2020-01-19 23:42:18 +0000

    pascal triangle

---
+++ cpp/src/array/generate.cc
@@ -0,0 +1,41 @@
+#include "test.h"
+/** Question no 268 easy - Missing Number
+ * Author : Li-Han, Chen; 陳立瀚
+ * Date   : 19th, January, 2020
+ * Source : https://leetcode.com/problems/pascals-triangle/
+ * 
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ * 
+*/
+
+/** Solution
+ * Runtime 0 ms	MeMory 8.8 MB; 
+ * faster than 100.00%, less than 82.35%
+ * O(n^2) ; O(n^2)
+*/
+
+std::vector<std::vector<int>> generate(int numRows) {
+    std::vector<std::vector<int>> ans;
+    
+    for (int i=0; i<numRows; i++) {
+      std::vector<int> vec;
+      if (i == 0) {
+        vec.push_back({1});
+      } else {
+        for (int j=0; j<ans[i-1].size(); j++) {
+          if (j==0) {
+            vec.push_back(ans[i-1][j]);
+          } else {
+            vec.push_back(ans[i-1][j] + ans[i-1][j-1]);
+          }
+        }
+        vec.push_back(1);
+      }
+      
+      ans.push_back(vec);
+    }
+    
+    return ans;
+  }
+
+
---
+++ cpp/src/str/maximum69Number.cc
@@ -0,0 +1,30 @@
+#include "test.h"
+/** Question no 268 easy - Missing Number
+ * Author : Li-Han, Chen; 陳立瀚
+ * Date   : 19th, January, 2020
+ * Source : https://leetcode.com/problems/maximum-69-number/
+ * 
+ * Given a positive integer num consisting only of digits 6 and 9.
+ * 
+ * Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+ * 
+*/
+
+/** Solution
+ * Runtime 0 ms	MeMory 8.8 MB; 
+ * faster than 100.00%, less than 82.35%
+ * O(n) ; O(1)
+*/
+
+int maximum69Number (int num) {
+  std::string numStr = std::to_string(num);
+  for (int i=0; i < numStr.size(); i++) {
+    if (numStr[i] == '6') {
+      numStr[i] = '9';
+      return std::stoi(numStr);
+    }
+  }
+  return num;
+}
+
+
