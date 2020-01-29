b349e5fa4cd46506bd4ccc914a5779c8c4b2120f
lhc37 <eden.chen@bath.edu>
2020-01-29 16:12:18 +0000

    O 1 space for cyclic rotation

--- codility/lesson2/CyclicRotation.cc
+++ codility/lesson2/CyclicRotation.cc
@@ -1,10 +1,30 @@
 /** Resources: https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
  * Score: 100% 
- * O(n); O(n)
- * Space could be O(1)
  */
 
+/* Solution O(n), O(1) */
 vector<int> solution(vector<int> &A, int K) {
+    // write your code in C++14 (g++ 6.2.0)
+    int ct = 0, LEN = A.size();
+    
+    for (int pos=0 ; ct<LEN; pos++) {
+        int cur_pos = pos;
+        int prev = A[pos];
+        do {
+            int rotat_pos = (cur_pos+K)%LEN;
+            int tmp = A[rotat_pos];
+            A[rotat_pos] = prev;
+            
+            cur_pos = rotat_pos;
+            prev = tmp;
+            ct++;
+        } while(cur_pos != pos);
+    }
+    return A;
+}
+
+/* trivial solution O(n), O(n)*/
+vector<int> trivial_solution(vector<int> &A, int K) {
     int LEN = A.size();
     K %= LEN;
     if (K == 0)    return A;
