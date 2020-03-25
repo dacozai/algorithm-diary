664795a9f6ee2c3831de8ddbe788aae68b0b7707
dacozai <eden.chen@bath.edu>
2020-03-25 20:47:26 +0800

    q 657

---
+++ cpp/src/ow/judgeCircle.cc
@@ -0,0 +1,25 @@
+#include "test.h"
+/** Question no 657 easy Robot Return to Origin
+ * Author : Li-Han, Chen; 陳立瀚
+ * Date   : 25th, March, 2020 
+ * Source : https://leetcode.com/problems/robot-return-to-origin/
+ * 
+ * 
+*/
+
+/** Solution
+ * Runtime 0  ms	MeMory 8.3 MB; 
+ * faster than 100.00%, less than 100.00%
+ * O(n) ; O(1)
+*/
+
+bool judgeCircle(std::string moves) {
+  if (moves.size()%2==1)return false;
+  int n=0;
+  for (int i=0;i<moves.size()/2;i++) {
+    if (moves[i] == 'R' && moves[])
+  }
+  return n==0;
+}
+
+
